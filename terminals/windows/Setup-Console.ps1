40c202c4c45b5a6283fe169035e370c6b53b2d32
James Sturtevant <jsturtevant@gmail.com>
2018-03-22 14:26:05 -0700

    adding tls 1.2 configuration

--- terminals/windows/Setup-Console.ps1
+++ terminals/windows/Setup-Console.ps1
@@ -15,6 +15,7 @@ function Install-Font()
 }
 
 # Install the DejaVu Sans Mono for Powerline font
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest `
   -Uri 'https://github.com/powerline/fonts/raw/master/DejaVuSansMono/DejaVu%20Sans%20Mono%20for%20Powerline.ttf' `
   -UseBasicParsing -OutFile `
